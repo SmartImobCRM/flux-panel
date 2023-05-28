e646b4608af8df4b7037779babe8ba18c1bcc055
Alexandre <47799117+xandykati98@users.noreply.github.com>
2023-05-27 22:04:06 -0300

    fim6

--- src/pages/api/createAction.ts
+++ src/pages/api/createAction.ts
@@ -1,42 +1,16 @@
 import { Prisma } from "@prisma/client";
 import { NextApiRequest, NextApiResponse } from "next";
+import NextCors from "nextjs-cors";
 import { prisma } from "~/server/db";
-import Cors from 'cors'
 
-// Initializing the cors middleware
-// You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
-const cors = Cors({
-  methods: ['POST', 'GET', 'HEAD'],
-})
-
-// Helper method to wait for a middleware to execute before continuing
-// And to throw an error when an error happens in a middleware
-function runMiddleware(
-  req: NextApiRequest,
-  res: NextApiResponse,
-  fn: (req: Cors.CorsRequest, res: {
-    statusCode?: number | undefined;
-    setHeader(key: string, value: string): any;
-    end(): any;
-}, next: (err?: any) => any) => void
-) {
-  return new Promise((resolve, reject) => {
-    fn(req, res, (result: any) => {
-      if (result instanceof Error) {
-        return reject(result)
-      }
-
-      return resolve(result)
-    })
-  })
-}
-
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  // Run the middleware
-  await runMiddleware(req, res, cors)
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+    
+   await NextCors(req, res, {
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    origin: '*',
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+ });
+ 
     if (!req.body) return res.status(400).json({ok:false})
 
     const body = req.body as { [key:string]:any };
@@ -52,3 +26,4 @@ export default async function handler(
         res.send(error)
     }
 }
+export default handler;
--- src/pages/api/external.ts
+++ src/pages/api/external.ts
@@ -1,6 +1,13 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import NextCors from "nextjs-cors";
 
-const handler = (req: NextApiRequest, res: NextApiResponse) => {
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+    
+   await NextCors(req, res, {
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    origin: '*',
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+ });
     res.status(200).json({ name: 'John Doe' })
 }
 export default handler;
