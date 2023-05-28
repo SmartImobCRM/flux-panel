9f220e3e34e205da8cd7214352198e0c3c653489
Alexandre <47799117+xandykati98@users.noreply.github.com>
2023-05-27 21:49:28 -0300

    fim4

--- src/pages/api/createAction.ts
+++ src/pages/api/createAction.ts
@@ -1,8 +1,38 @@
 import { Prisma } from "@prisma/client";
 import { NextApiRequest, NextApiResponse } from "next";
 import { prisma } from "~/server/db";
+import Cors from 'cors'
 
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+// Initializing the cors middleware
+// You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+const cors = Cors({
+  methods: ['POST', 'GET', 'HEAD'],
+})
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  fn: Function
+) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: any) => {
+      if (result instanceof Error) {
+        return reject(result)
+      }
+
+      return resolve(result)
+    })
+  })
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  // Run the middleware
+  await runMiddleware(req, res, cors)
     if (!req.body) return res.status(400).json({ok:false})
 
     const body = req.body as { [key:string]:any };
@@ -18,4 +48,3 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
         res.send(error)
     }
 }
-export default handler;
--- src/pages/api/createMigracao.ts
+++ src/pages/api/createMigracao.ts
@@ -1,8 +1,38 @@
 import { Prisma } from "@prisma/client";
 import { NextApiRequest, NextApiResponse } from "next";
 import { prisma } from "~/server/db";
+import Cors from 'cors'
 
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+// Initializing the cors middleware
+// You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+const cors = Cors({
+  methods: ['POST', 'GET', 'HEAD'],
+})
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  fn: Function
+) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: any) => {
+      if (result instanceof Error) {
+        return reject(result)
+      }
+
+      return resolve(result)
+    })
+  })
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  // Run the middleware
+  await runMiddleware(req, res, cors)
     const body = req.body as { [key:string]:any };
     try {
         const Migracao = await prisma.migracao.create({
@@ -15,4 +45,3 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
         res.send(error)
     }
 }
-export default handler;
